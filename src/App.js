87a18c58e5f639de0e76fd69084094f65cb72fc3
webdesign4alll <190048@tkmce.ac.in>
2021-10-18 00:27:14 +0530

    added new render function to the menucomponent.js

--- src/App.js
+++ src/App.js
@@ -1,47 +1,45 @@
-import React, {Component} from  'react';
-import {Collapse, Nav, Navbar, NavbarBrand, NavItem, NavLink} from 'reactstrap';
-import Menu from './components/MenuComponent';
-import {DISHES} from './shared/dishes';
-import './App.css';
+import React, { Component } from "react";
+import { Nav, Navbar, NavbarBrand, NavItem, NavLink } from "reactstrap";
+import Menu from "./components/MenuComponent";
+import { DISHES } from "./shared/dishes";
+import "./App.css";
 
-class App extends Component{
-
-  constructor(props){
+class App extends Component {
+  constructor(props) {
     super(props);
     this.state = {
-      dishes : DISHES
-    }
+      dishes: DISHES,
+    };
   }
 
-render(){
-  return (
-    <div className="App">
-   <Navbar dark color = 'primary'>
-     <div className = "container">
-       <NavbarBrand href = "/">Ristorante Con Fusion</NavbarBrand>
-     </div>
-     <Nav navbar>
-       <NavItem>
-         <NavLink href = "www.youtube.com">Youtube</NavLink>
-       </NavItem>
+  render() {
+    return (
+      <div className="App">
+        <Navbar dark color="primary">
+          <div className="container">
+            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+          </div>
+          <Nav navbar>
+            <NavItem>
+              <NavLink href="www.youtube.com">Youtube</NavLink>
+            </NavItem>
 
-       <NavItem>
-         <NavLink href = "www.github.com">github</NavLink>
-       </NavItem>
+            <NavItem>
+              <NavLink href="www.github.com">github</NavLink>
+            </NavItem>
 
-       <NavItem>
-         <NavLink href = "www.youtube.com">projects</NavLink>
-       </NavItem>
-       
-        </Nav>
-   </Navbar>
+            <NavItem>
+              <NavLink href="www.youtube.com">projects</NavLink>
+            </NavItem>
+          </Nav>
+        </Navbar>
 
-   <div className = "container-fluid">
-     <Menu dishes = {this.state.dishes}/>
-   </div>
-    </div>
-  );
-}
+        <div className="container-fluid">
+          <Menu dishes={this.state.dishes} />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,44 +1,68 @@
-import React, { Component } from 'react';
-import { Media } from 'reactstrap';
+import React, { Component } from "react";
+import {
+  Card,
+  CardBody,
+  CardImg,
+  CardImgOverlay,
+  CardTitle,
+  CardText,
+} from "reactstrap";
 
 class Menu extends Component {
-  
-
-    render(){
-
-        const menu = this.props.dishes.map((dish) =>{
-    
-         return(
-            <div className = "container">
-          
-                <Media tag = "li" className = "mt-5 text-center">
-                    <Media left middle>
-                        <Media object src = {dish.image}></Media>
-                    </Media>
-                    <Media body>
-                        <Media heading >{dish.name}</Media>
-                        <p>{dish.description}</p>
-                    </Media>
-                </Media>
-     
-        </div>
-         )
-        })
-    
-        return (
-    
-            <div className="container">
-    
-                <Media list>
-                    {menu}
-                </Media>
-            </div>
-    
-        );
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedDish: null,
     };
-    
+  }
+
+  onDishSelect(dish) {
+    this.setState({
+      selectedDish: dish,
+    });
+  }
+
+
+  renderDish(dish){
+        if(dish != null){
+         return(
+            <Card className="m-1 col-md-12">
+            <CardImg width="100%" src={dish.image} alt={dish.name} />
+  
+            <CardImgOverlay>
+              <CardTitle>{dish.name}</CardTitle>
+            </CardImgOverlay>
+            <CardBody><CardText>{dish.description}</CardText></CardBody>
+          </Card>
+         )
+
+        }else{
+            return(
+                <div></div>
+            )
+        }
+  }
+
+  render() {
+    const menu = this.props.dishes.map((dish) => {
+      return (
+        <Card className="m-1 col-md-2" onClick={() => this.onDishSelect(dish)}>
+          <CardImg width="100%" src={dish.image} alt={dish.name} />
+
+          <CardImgOverlay>
+            <CardTitle>{dish.name}</CardTitle>
+          </CardImgOverlay>
+        </Card>
+      );
+    });
+
+    return (
+      <div className="container mt-5 bg-secondary">
+        <div className="row">{menu}</div>
+        <div className="container">{this.renderDish(this.state.selectedDish)}</div>
+      </div>
+    );
+  }
 }
-
-
 
 export default Menu;
